24c11d10e6fabf5a9efb16a607c4b6ab673cd9ed
Gabby Mendoza <Hachiko97@users.noreply.github.com>
2019-06-21 19:12:08 +1000

    Centering the form

--- page-contact.php
+++ page-contact.php
@@ -15,13 +15,13 @@ get_header(); ?>
 		<section class="">
 			<div class="container">
 			<div class="row">
-			<div class="col-12">
+			<div class="col-12 text-center">
 			<h2 class="section-header mb-4">
 				Contact Us
 			</h2>
 			<p>Got a question? Drop your question the form below and we'll get back to you as soon as possible</p>
 			</div>
-			<div class="col-12">
+			<div class="col-12 text-center mx-auto">
 			<?php echo do_shortcode('[contact-form-7 id="25" title="Contact form 1"]');?>
 			</div>
 			</div>
