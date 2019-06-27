44e1e9feebe420803e91f996c08a6e08e5ac5ab0
Gabby Mendoza <mendozagabby19@gmail.com>
2019-06-27 11:27:34 +1000

    Update testimonials.php

--- inc/components/testimonials.php
+++ inc/components/testimonials.php
@@ -9,7 +9,7 @@
               <div class="col-12 col-lg-6 my-auto pr-4 order-2 order-lg-1">
                 <h3 class="testimonials__title">So many great features!</h3>
                 <p class="testimonials__review">
-                  "Website looks really slick and I love that I can apply when it suits me. It’s also good to know that they take security seriously, so I don’t have to worry. So many great features!"
+                  "Website looks really slick and I love that I can secure a loan when it suits me. It’s also good to know that they take security seriously, so I don’t have to worry. So many great features!"
                 </p>
                 <span class="testimonials__reviewer bold">Wes A, <span class="font-weight--400">Conneticut</span></span>
               </div>
@@ -24,7 +24,7 @@
               <div class="col-12 col-lg-6 my-auto pr-4 order-2 order-lg-1">
                 <h3 class="testimonials__title">So many great features!</h3>
                 <p class="testimonials__review">
-                  "Website looks really slick and I love that I can apply when it suits me. It’s also good to know that they take security seriously, so I don’t have to worry. So many great features!"
+                  "Website looks really slick and I love that I can secure a loan when it suits me. It’s also good to know that they take security seriously, so I don’t have to worry. So many great features!"
                 </p>
               </div>
               <div class="col-12 col-lg-6 order-1 order-lg-2 mb-4 mb-lg-0">
@@ -38,7 +38,7 @@
               <div class="col-12 col-lg-6 my-auto pr-4 order-2 order-lg-1">
                 <h3 class="testimonials__title">So many great features!</h3>
                 <p class="testimonials__review">
-                  "Website looks really slick and I love that I can apply when it suits me. It’s also good to know that they take security seriously, so I don’t have to worry. So many great features!"
+                  "Website looks really slick and I love that I can secure a loan when it suits me. It’s also good to know that they take security seriously, so I don’t have to worry. So many great features!"
                 </p>
               </div>
               <div class="col-12 col-lg-6 order-1 order-lg-2 mb-4 mb-lg-0">
